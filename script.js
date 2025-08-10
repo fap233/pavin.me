ce264d86dd7e7bd05b619c2302b98c0189b9c1f4
Fellipe Pavin <127888286+fap233@users.noreply.github.com>
2025-08-10 22:06:23 +0000

    first deploy of my portfolio static page

---
+++ script.js
@@ -0,0 +1,3 @@
+// Este arquivo JavaScript está aqui para manter a estrutura, mas não executa nenhuma ação no momento.
+// Ele será compartilhado entre index.html e portfolio.html.
+console.log("Scripts carregados com sucesso.");
