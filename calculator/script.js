1fe035a5d753edb2a6aa4df32c8607baba306432
fap233 <fellipe306@gmail.com>
2025-09-07 17:39:20 -0300

    feat: keyboard support

--- calculator/script.js
+++ calculator/script.js
@@ -1,11 +1,37 @@
-"use strict";
-
 const display = document.getElementById("display");
+
 const numbers = document.querySelectorAll("[id*=key]");
+
 const operators = document.querySelectorAll("[id*=Operator]");
 
+const keyMap = {
+	0: "key0",
+	1: "key1",
+	2: "key2",
+	3: "key3",
+	4: "key4",
+	5: "key5",
+	6: "key6",
+	7: "key7",
+	8: "key8",
+	9: "key9",
+	"/": "divOperator",
+	"*": "multOperator",
+	"-": "minusOperator",
+	"+": "plusOperator",
+	"=": "equals",
+	Enter: "equals",
+	Backspace: "backspace",
+	c: "clearEntry",
+	Escape: "clear",
+	",": "decimal",
+	".": "decimal",
+};
+
 let newNumber = true;
+
 let operator;
+
 let previousNumber;
 
 const pendentOperation = () => operator !== undefined;
@@ -13,44 +39,66 @@ const pendentOperation = () => operator !== undefined;
 const calculate = () => {
 	if (pendentOperation()) {
 		const actualNumber = parseFloat(display.textContent);
+
 		newNumber = true;
 
 		let result;
+
 		switch (operator) {
 			case "+":
 				result = previousNumber + actualNumber;
+
 				break;
+
 			case "-":
 				result = previousNumber - actualNumber;
+
 				break;
+
 			case "*":
 				result = previousNumber * actualNumber;
+
 				break;
+
 			case "/":
 				result = previousNumber / actualNumber;
+
 				break;
 		}
 
-		/*	const result = eval(`${previousNumber}${operator}${actualNumber}`);
-		displayUpdate(result);
+		/*    const result = eval(`${previousNumber}${operator}${actualNumber}`);
 
-		/*	if (operator == "+") {
-			displayUpdate(previousNumber + actualNumber);
-		} else if (operator == "*") {
-			displayUpdate(previousNumber * actualNumber);
-		} else if (operator == "-") {
-			displayUpdate(previousNumber - actualNumber);
-		} else if (operator == "/") {
-			displayUpdate(previousNumber / actualNumber);
-		}
+        displayUpdate(result);
+
+
+        /*    if (operator == "+") {
+
+            displayUpdate(previousNumber + actualNumber);
+
+        } else if (operator == "*") {
+
+            displayUpdate(previousNumber * actualNumber);
+
+        } else if (operator == "-") {
+
+            displayUpdate(previousNumber - actualNumber);
+
+        } else if (operator == "/") {
+
+            displayUpdate(previousNumber / actualNumber);
+
+        }
+
   */
-		displayUpdate(result);
+
+		displayUpdate(parseFloat(result.toFixed(4)));
 	}
 };
 
 const displayUpdate = (text) => {
 	if (newNumber) {
 		display.textContent = text;
+
 		newNumber = false;
 	} else {
 		display.textContent += text;
@@ -64,9 +112,14 @@ numbers.forEach((number) => number.addEventListener("click", insertNumber));
 const selectOperator = (event) => {
 	if (!newNumber) {
 		calculate();
+
 		newNumber = true;
+
 		operator = event.target.textContent;
+
 		previousNumber = parseFloat(display.textContent);
+
+		display.textContent = `${previousNumber} ${operator}`;
 	}
 };
 
@@ -76,9 +129,12 @@ operators.forEach((operator) =>
 
 const activateEquals = () => {
 	calculate();
+
 	operator = undefined;
+
 	newNumber = true;
 };
+
 document.getElementById("equals").addEventListener("click", activateEquals);
 
 // clear display (CE)
@@ -93,9 +149,53 @@ document.getElementById("clearEntry").addEventListener("click", clearEntry);
 
 const clear = () => {
 	clearEntry();
+
 	newNumber = true;
+
 	operator = undefined;
+
 	previousNumber = undefined;
 };
 
 document.getElementById("clear").addEventListener("click", clear);
+
+const backspace = () => {
+	display.textContent = display.textContent.slice(0, -1);
+};
+
+document.getElementById("backspace").addEventListener("click", backspace);
+
+const invertSignal = () => {
+	newNumber = true;
+
+	displayUpdate(display.textContent * -1);
+};
+
+document.getElementById("invert").addEventListener("click", invertSignal);
+
+const decimalPoint = () => {
+	const parts = display.textContent.split(" ");
+
+	const lastPart = parts[parts.length - 1];
+
+	if (!lastPart.includes(".")) {
+		if (lastPart === "" || lastPart.includes(operator)) {
+			displayUpdate("0.");
+		} else {
+			displayUpdate(".");
+		}
+	}
+};
+
+document.getElementById("decimal").addEventListener("click", decimalPoint);
+
+const activateButton = (event) => {
+	const key = event.key;
+	const targetButtonId = keyMap[key];
+
+	if (targetButtonId) {
+		document.getElementById(targetButtonId).click();
+	}
+};
+
+window.addEventListener("keydown", activateButton);
