4c2aabc591f850b24d7675bc5ba373920a5b9406
fap233 <fellipe306@gmail.com>
2025-09-09 14:55:48 -0300

    feat: todolist complete version

--- todolist/script.js
+++ todolist/script.js
@@ -1,18 +1,16 @@
 "use strict";
 
-let database = [
-	{ task: "Estudar JS", status: "" },
-	{ task: "Netflix", status: "checked" },
-	{ task: "teste", status: "checked" },
-];
+const getDatabase = () => JSON.parse(localStorage.getItem("todoList")) ?? [];
+const setDatabase = (database) =>
+	localStorage.setItem("todoList", JSON.stringify(database));
 
-const createItem = (task, status) => {
+const createItem = (task, status, index) => {
 	const item = document.createElement("label");
 	item.classList.add("todo__item");
 	item.innerHTML = `
-          <input type="checkbox" ${status}/>
+          <input type="checkbox" ${status} data-index=${index}/>
           <div>${task}</div>
-          <input type="button" value="X" />
+          <input type="button" value="X" data-index=${index} />
 `;
 
 	document.getElementById("todoList").appendChild(item);
@@ -27,7 +25,48 @@ const clearTasks = () => {
 
 const screenUpdate = () => {
 	clearTasks();
-	database.forEach((item) => createItem(item.task, item.status));
+	const database = getDatabase();
+	database.forEach((item, index) => createItem(item.task, item.status, index));
 };
+
+const insertItem = (event) => {
+	const key = event.key;
+	const text = event.target.value;
+	const clearInput = () => (event.target.value = "");
+	if (key === "Enter") {
+		const database = getDatabase();
+		database.push({ task: text, status: "" });
+		setDatabase(database);
+		screenUpdate();
+		clearInput();
+	}
+};
+
+const removeItem = (index) => {
+	const database = getDatabase();
+	database.splice(index, 1);
+	setDatabase(database);
+	screenUpdate();
+};
+
+const updateItem = (index) => {
+	const database = getDatabase();
+	database[index].status = database[index].status === "" ? "checked" : "";
+	setDatabase(database);
+	screenUpdate();
+};
+
+const clickItem = (event) => {
+	const element = event.target;
+	const index = parseInt(element.dataset.index, 10);
+	if (element.type === "button") {
+		removeItem(index);
+	} else if (element.type === "checkbox") {
+		updateItem(index);
+	}
+};
+
+document.getElementById("newItem").addEventListener("keypress", insertItem);
+document.getElementById("todoList").addEventListener("click", clickItem);
 
 screenUpdate();
