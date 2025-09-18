3c3a8b6f9e5b101f8966e9f7a90b9ad02911760f
fap233 <fellipe306@gmail.com>
2025-09-17 21:34:11 -0300

    update(figma-to-website): dragon ball landing pag js and css updates.

--- figma-to-website/dragonball/js/main.js
+++ figma-to-website/dragonball/js/main.js
@@ -3,4 +3,18 @@
 const body = document.querySelector("body");
 const video = document.querySelector("video");
 
-body.onclick = () => (video.muted = !video.muted);
+// body.onclick = () => (video.muted = !video.muted);
+
+body.onclick = (event) => {
+	if (event.target.closest("a")) {
+		return;
+	}
+
+	video.muted = !video.muted;
+};
+
+const menu = document.querySelector(".menu-burger-container");
+
+const active = () => menu.classList.toggle("active");
+
+menu.addEventListener("click", active);
